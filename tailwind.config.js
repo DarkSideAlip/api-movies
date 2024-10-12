84b487e362a0a3fb9064f9119f498f868e67e6bd
DarkSideAlip <01tanwaalip@gmail.com>
2024-10-12 08:15:56 +0700

    Your commit message

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // รวมไฟล์ที่ Tailwind จะทำงาน
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
